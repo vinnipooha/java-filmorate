0c174e148ed0955220d7372a435e5b621f3083ae
Marina Sheremet <vinnipooha@yandex.ru>
2024-04-27 07:49:08 +0700

    TS11: FIX_устранены неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
